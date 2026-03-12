23d68e08ca1d6087ed2ffe8e99634e1097931c74
Sunny <sunnysinghhrana@gmail.com>
2026-03-12 22:34:39 +0530

    Add - LogEntry

--- src/main/java/com/logtool/model/LogEntry.java
+++ src/main/java/com/logtool/model/LogEntry.java
@@ -1,5 +1,60 @@
 package com.logtool.model;
 
-public class LogEntry {
+import java.time.LocalDateTime;
 
+public class LogEntry {
+    private long id;
+    private LocalDateTime timestamp;
+    private String level;
+    private String service;
+    private String message;
+    private String fileName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
