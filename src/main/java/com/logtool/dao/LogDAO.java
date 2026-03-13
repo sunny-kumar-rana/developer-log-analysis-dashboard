e402ea9497ac95b29dd3ad2e5c7f1c1a2844ce77
Sunny <sunnysinghhrana@gmail.com>
2026-03-13 23:30:28 +0530

    Add - required methods in LogDAO.java

--- src/main/java/com/logtool/dao/LogDAO.java
+++ src/main/java/com/logtool/dao/LogDAO.java
@@ -1,5 +1,27 @@
 package com.logtool.dao;
 
-public class LogDAO {
+import com.logtool.model.LogEntry;
 
+import java.sql.Connection;
+import java.util.List;
+
+public class LogDAO {
+    public boolean save(Connection conn, LogEntry lg){
+
+    }
+    public boolean saveBatch(Connection conn, List<LogEntry> logs){
+
+    }
+    public List<LogEntry> findRecentErrors(Connection conn){
+
+    }
+    public List<LogEntry> countByLevel(Connection conn){
+
+    }
+    public List<LogEntry> countErrorsByService(Connection conn){
+
+    }
+    public List<LogEntry> searchLogs(Connection conn){
+
+    }
 }
