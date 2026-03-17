71670275eb43daaad3dadfd0052d5e0011db9bc0
Sunny <sunnysinghhrana@gmail.com>
2026-03-17 22:26:17 +0530

    Add - SearchServlet.java

---
+++ src/main/java/com/logtool/servlet/SearchServlet.java
@@ -0,0 +1,26 @@
+package com.logtool.servlet;
+
+import com.logtool.service.LogService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SearchServlet extends HttpServlet {
+
+    LogService ls = new LogService();
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+        try{
+
+            String keyword = request.getParameter("q");
+
+            request.setAttribute("logs", ls.searchLogs(keyword));
+
+            request.getRequestDispatcher("search.jsp").forward(request, response);
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
